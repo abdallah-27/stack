6ed019c5b605c36e1791dbaeda7009c1338efa51
abdallah-27 <abdallaha.gueye@gmail.com>
2022-02-27 22:36:21 +0100

    push & pop

--- stack.js
+++ stack.js
@@ -6,9 +6,24 @@ class Stack {
     }
 
     
+  // Adding element to the stack
+  push(elements){
+    this.items[this.count] = elements; // adding first element
+    this.count += 1;
+    console.log(`${elements} add to ${this.count}`); 
+    return this.count -1; // returning the position
+    }
 
 
 }
+// creating stack object
+const stack = new Stack();
+
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
 
 
 
